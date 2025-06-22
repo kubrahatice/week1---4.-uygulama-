b93f878bae1bea0e077e5a1eab76aa9d5e719936
kubrahatice <kubra_bkc@hotmail.com>
2025-06-22 11:28:24 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+        // Kullanıcıya soru sor
+        Console.Write("Kübra manavına hoşgeldiniz!");
+
+        Console.Write("Hangi meyveyi satın almak istiyorsunuz? ");
+        string meyve = Console.ReadLine().ToLower(); // Küçük harfe çevirerek karşılaştırma yapıyoruz
+
+        int fiyat;
+
+        switch (meyve)
+        {
+            case "elma":
+                fiyat = 2;
+                break;
+            case "armut":
+                fiyat = 3;
+                break;
+            case "çilek":
+                fiyat = 2;
+                break;
+            case "muz":
+                fiyat = 3;
+                break;
+            default:
+                fiyat = 4; // Diğer bütün meyveler
+                break;
+        }
+
+        // Sonucu ekrana yazdır
+        Console.WriteLine($"{meyve} fiyatı: {fiyat} TL");
+
+        // Program hemen kapanmasın diye bekletme
+        Console.WriteLine("Çıkmak için bir tuşa basınız...");
+        Console.ReadKey();
+    }
+}
